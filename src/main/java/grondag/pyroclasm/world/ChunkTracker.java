2a906415e4ec14aa96c21de45e7a86587a97de45
grondag <grondag@gmail.com>
2018-10-10 19:40:52 -0700

    improve concurrency

--- src/main/java/grondag/pyroclasm/world/ChunkTracker.java
+++ src/main/java/grondag/pyroclasm/world/ChunkTracker.java
@@ -1,51 +1,69 @@
 package grondag.pyroclasm.world;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.Nullable;
+
 import grondag.exotic_matter.simulator.ChunkLoader;
-import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
-import it.unimi.dsi.fastutil.longs.LongArrayList;
 import net.minecraft.world.World;
 
 public class ChunkTracker
 {
-    private final LongArrayList list = new LongArrayList();
+    private final ConcurrentHashMap<Long, AtomicInteger> map = new ConcurrentHashMap<>();
     
-    private final Long2IntOpenHashMap map = new Long2IntOpenHashMap();
+    @Nullable Iterator<Map.Entry<Long, AtomicInteger>> iterator = null;
     
-    int nextPos = 0;
+    private AtomicInteger trackedCount = new AtomicInteger();
     
     public void clear()
     {
-        this.list.clear();
         this.map.clear();
+        this.trackedCount.set(0);
     }
     
     public int size()
     {
-        return this.list.size();
+        return this.trackedCount.get();
     }
     
     public void trackChunk(World world, long packedChunkPos)
     {
-        if(map.addTo(packedChunkPos, 1) == 0)
+        final AtomicInteger count = map.computeIfAbsent(packedChunkPos, k -> {return new AtomicInteger();});
+        if(count.incrementAndGet() == 1)
         {
-            list.add(packedChunkPos);
             ChunkLoader.retainChunk(world, packedChunkPos);
+            trackedCount.incrementAndGet();
         }
     }
     
     public void untrackChunk(World world, long packedChunkPos)
     {
-        if(map.addTo(packedChunkPos, -1) == 1)
+        final AtomicInteger count = map.computeIfAbsent(packedChunkPos, k -> {return new AtomicInteger();});
+        if(count.decrementAndGet() == 0)
         {
-            list.rem(packedChunkPos);
             ChunkLoader.releaseChunk(world, packedChunkPos);
+            trackedCount.decrementAndGet();
         }
     }
     
     public long nextPackedChunkPosForUpdate()
     {
-        if(this.list.isEmpty()) return 0;
-        if(this.nextPos >= this.list.size()) this.nextPos = 0;
-        return this.list.getLong(nextPos++);
+        Iterator<Map.Entry<Long, AtomicInteger>> it = this.iterator;
+        if(it == null || !it.hasNext())
+        {
+            it = this.map.entrySet().iterator();
+            this.iterator = it;
+        }
+        
+        while(it.hasNext())
+        {
+            Map.Entry<Long, AtomicInteger> e = it.next();
+            if(e.getValue().get() > 0)
+                return e.getKey();
+        }
+        return 0;
     }
 }
