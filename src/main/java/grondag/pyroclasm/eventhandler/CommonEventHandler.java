bc806afd2f667a484c89c18ad576dd09b06839b0
grondag <grondag@gmail.com>
2018-10-11 08:21:46 -0700

    Remove abuse factory now that activation logic is implemented

--- src/main/java/grondag/pyroclasm/eventhandler/CommonEventHandler.java
+++ src/main/java/grondag/pyroclasm/eventhandler/CommonEventHandler.java
@@ -1,21 +1,13 @@
 package grondag.pyroclasm.eventhandler;
 
-import javax.annotation.Nullable;
-
 import grondag.exotic_matter.simulator.Simulator;
-import grondag.exotic_matter.world.WorldInfo;
+import grondag.pyroclasm.Configurator;
 import grondag.pyroclasm.Pyroclasm;
 import grondag.pyroclasm.volcano.VolcanoManager;
-import grondag.pyroclasm.Configurator;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.config.Config.Type;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -55,57 +47,6 @@ public class CommonEventHandler
         VolcanoManager vm = Simulator.instance().getNode(VolcanoManager.class);
         if(vm != null)
             vm.handleChunkUnload(event.getWorld(), event.getChunk());
-    }
-    
-    //TODO: make these player caps, vs static global
-    private static @Nullable String lastTroubleMaker = null;
-    private static @Nullable BlockPos lastAttemptLocation = null;
-    private static long lastAttemptTimeMillis = -1;
-    private static int attemptsAtTrouble = 0;
-    
-    @SubscribeEvent
-    public static void onAskingForIt(ServerChatEvent event)
-    {
-        EntityPlayerMP player = event.getPlayer();
-        
-        if(player.getHeldItemMainhand().getItem() == Items.LAVA_BUCKET && event.getMessage().toLowerCase().contains("volcano"))
-        {
-            long time = WorldInfo.currentTimeMillis();
-
-            if(event.getUsername() == lastTroubleMaker
-                    && player.getPosition().equals(lastAttemptLocation)
-                    && time - lastAttemptTimeMillis < 30000)
-            {
-                //FIXME" check for volcano nearby
-                
-                attemptsAtTrouble++;
-                
-                //FIXME: localize
-                if(attemptsAtTrouble == 1)
-                {
-                    player.sendMessage(new TextComponentString(String.format("This is a really bad idea, %s", player.getDisplayNameString())));
-                }
-                else if(attemptsAtTrouble == 2)
-                {
-                    player.sendMessage(new TextComponentString(String.format("I hope there isn't anything nearby you want to keep.", player.getDisplayNameString())));
-                }
-                else if(attemptsAtTrouble == 3)
-                {
-                    player.sendMessage(new TextComponentString(String.format("Now would be a good time to run away.", player.getDisplayNameString())));
-                    // FIXME: call volcano wake now that volcano block isnt a thing
-//                    player.world.setBlockState(new BlockPos(lastAttemptLocation.getX(), 0, lastAttemptLocation.getZ()), ModBlocks.volcano_block.getDefaultState());
-                }
-            }
-            else
-            {
-                attemptsAtTrouble = 0;
-            }
-            lastTroubleMaker = event.getUsername();
-            lastAttemptLocation = player.getPosition();
-            lastAttemptTimeMillis = time;
-            
-            Pyroclasm.INSTANCE.warn("player is asking for it at " + event.getPlayer().posX + " " + event.getPlayer().posZ);
-        }
     }
     
     @SubscribeEvent
