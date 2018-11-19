c1eaf0e7589694468abc693761a74ede2836509d
grondag <grondag@gmail.com>
2018-11-19 11:01:01 -0800

    Reorg primitives

--- src/main/java/grondag/pyroclasm/block/VertexProcessorLavaAdvanced.java
+++ src/main/java/grondag/pyroclasm/block/VertexProcessorLavaAdvanced.java
@@ -4,7 +4,7 @@ import grondag.exotic_matter.model.painting.PaintLayer;
 import grondag.exotic_matter.model.painting.VertexProcessor;
 import grondag.exotic_matter.model.painting.VertexProcessors;
 import grondag.exotic_matter.model.primitives.IPaintableQuad;
-import grondag.exotic_matter.model.primitives.IPaintableVertex;
+import grondag.exotic_matter.model.primitives.vertex.IPaintableVertex;
 import grondag.exotic_matter.model.primitives.QuadHelper;
 import grondag.exotic_matter.model.state.ISuperModelState;
 import grondag.exotic_matter.terrain.IHotBlock;
